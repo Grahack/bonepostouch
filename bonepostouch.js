026add8f44a3061301a9b0cf930fc7f37c6a6292
Christophe Gragnic <cgragnic@netc.fr>
2024-04-16 17:48:53 +0200

    Actual notes

--- bonepostouch.js
+++ bonepostouch.js
@@ -1,12 +1,47 @@
+var partial = 1;
+
+var notes = [
+    // partial 1
+    ["Bb/A#", "A", "Ab/G#", "G", "F#/Gb", "F", "E"],
+    // partial 2
+    ["F", "E", "Eb/D#", "D", "C#/Db", "C", "B"],
+    // partial 3
+    ["Bb/A#", "A", "Ab/G#", "G", "F#/Gb", "F", "E"],
+    // partial 4
+    ["D", "C#/Db", "C", "B", "Bb/A#", "A", "Ab/G#"],
+    // partial 5
+    ["F", "E", "Eb/D#", "D", "C#/Db", "C", "B"],
+    // partial 6
+    ["Ab/G#", "G", "F#/Gb", "F", "E", "Eb/D#", "D"],
+    // partial 7
+    ["Bb/A#", "A", "Ab/G#", "G", "F#/Gb", "F", "E"],
+    // partial 8
+    ["C", "B", "Bb/A#", "A", "Ab/G#", "G", "Gb/F#"],
+    // partial 9
+    ["D", "C#/Db", "C", "B", "Bb/A#", "A", "Ab/G#"],
+    // partial 10
+    ["E", "Eb/D#", "D", "C#/Db", "C", "B", "Bb/A#"]];
+
 function action(name) {
     const lastIndex = name.lastIndexOf('_');
     const type = name.slice(0, lastIndex);
     const number = name.slice(lastIndex + 1);
     switch (type) {
         case 'rect_pos':
-            return function () {alert('pos ' + number);};
+            return function () {
+                SVGs['note'].firstChild.textContent = notes[partial-1][number-1];
+            };
         case 'rect_partial':
-            return function () {alert('partial ' + number);};
+            return function () {
+                // partial navigation buttons:
+                // #1  up  #2
+                // #3 down #4
+                if (number <= 2 && partial < 10) partial = partial + 1;
+                if (number >= 3 && partial > 1)  partial = partial - 1;
+                console.log("Partial num:", partial);
+                SVGs['partial_num'].firstChild.textContent = partial;
+                SVGs['note'].firstChild.textContent = "";
+            };
     }
 }
 
@@ -15,6 +50,8 @@ const NS = 'http://www.w3.org/2000/svg';
 var main_frame = document.createElementNS(NS, 'svg');
 var SVGs = {
     main_line : document.createElementNS(NS, 'line'),
+    partial_num : document.createElementNS(NS, 'text'),
+    note : document.createElementNS(NS, 'text'),
     main_rect : document.createElementNS(NS, 'rect')
 };
 
@@ -77,6 +114,26 @@ function adjust_size() {
     SVGs['main_line'].setAttribute('x2', screen_W / 2);
     SVGs['main_line'].setAttribute('y2', screen_H - H/14);
 
+    SVGs['partial_num'].setAttribute('stroke', 'black');
+    SVGs['partial_num'].setAttribute('stroke-width', 1);
+    SVGs['partial_num'].setAttribute('stroke-linecap', 'round');
+    SVGs['partial_num'].setAttribute('x', screen_W / 2 - W/2 + W/6);
+    SVGs['partial_num'].setAttribute('y', screen_H / 2 - H/2 + 3*H/7);
+    SVGs['partial_num'].setAttribute('font-size', W / 15);
+    SVGs['partial_num'].setAttribute('text-anchor', 'middle');
+    var textNode = document.createTextNode(partial);
+    SVGs['partial_num'].appendChild(textNode);
+
+    SVGs['note'].setAttribute('stroke', 'black');
+    SVGs['note'].setAttribute('stroke-width', 1);
+    SVGs['note'].setAttribute('stroke-linecap', 'round');
+    SVGs['note'].setAttribute('x', screen_W / 2 + W/3);
+    SVGs['note'].setAttribute('y', screen_H / 2 - H/2 + 3*H/7);
+    SVGs['note'].setAttribute('font-size', W / 15);
+    SVGs['note'].setAttribute('text-anchor', 'middle');
+    var textNode = document.createTextNode("");
+    SVGs['note'].appendChild(textNode);
+
     for (let i = 1; i <= 2; i++) {
         var name = 'line_pos_' + i;
         var y = (i-1) * 2 * H/7 + H/3 + 4*margin;
@@ -93,7 +150,7 @@ function adjust_size() {
         var name = 'rect_pos_' + (8-i);
         SVGs[name].setAttribute('fill', 'red');
         SVGs[name].setAttribute('fill-opacity', 0);
-        SVGs[name].setAttribute('stroke', 'grey');
+        SVGs[name].setAttribute('stroke', 'none');
         SVGs[name].setAttribute('stroke-width', 1);
         SVGs[name].setAttribute('stroke-dasharray', '5, 15');
         SVGs[name].setAttribute('stroke-linecap', 'round');
@@ -111,7 +168,7 @@ function adjust_size() {
         var name = 'rect_partial_' + i;
         SVGs[name].setAttribute('fill', 'red');
         SVGs[name].setAttribute('fill-opacity', 0);
-        SVGs[name].setAttribute('stroke', 'grey');
+        SVGs[name].setAttribute('stroke', 'none');
         SVGs[name].setAttribute('stroke-width', 1);
         SVGs[name].setAttribute('stroke-dasharray', '5,15');
         SVGs[name].setAttribute('stroke-linecap', 'round');
